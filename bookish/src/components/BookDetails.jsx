316ecfcc7599e45af847b247f1a2bb8db55d79b1
andrew dillon <majik895@gmail.com>
2019-07-21 09:24:02 -0400

    Added Reducer

--- bookish/src/components/BookDetails.jsx
+++ bookish/src/components/BookDetails.jsx
@@ -4,7 +4,7 @@ import { BookContext } from './../context/BookContext'
 const BookDetails = ({ book }) => {
 	const { dispatch } = useContext(BookContext)
 	return (
-		<li onClick={() => dispatch({ type: 'REMOVE_BOOK' })}>
+		<li onClick={() => dispatch({ type: 'REMOVE_BOOK', id: book.id })}>
 			<div className="title"> {book.title} </div>
 			<div className="author"> {book.author} </div>
 		</li>
